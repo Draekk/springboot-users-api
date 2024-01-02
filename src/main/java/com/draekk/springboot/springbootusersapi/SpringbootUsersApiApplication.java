317e35b5f460f623087e61f63c48ea55c657a0d4
Draekk <rodriver1992@gmail.com>
2024-01-02 17:07:04 -0300

    Add initial project files

---
+++ src/main/java/com/draekk/springboot/springbootusersapi/SpringbootUsersApiApplication.java
@@ -0,0 +1,13 @@
+package com.draekk.springboot.springbootusersapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootUsersApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootUsersApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/draekk/springboot/springbootusersapi/SpringbootUsersApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.draekk.springboot.springbootusersapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootUsersApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
