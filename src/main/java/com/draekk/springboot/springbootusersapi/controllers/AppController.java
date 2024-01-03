548396db1e28827c0fdec3de84f2170f5838164b
Draekk <rodriver1992@gmail.com>
2024-01-03 19:44:57 -0300

    Add userlist endpoint to AppController

--- src/main/java/com/draekk/springboot/springbootusersapi/controllers/AppController.java
+++ src/main/java/com/draekk/springboot/springbootusersapi/controllers/AppController.java
@@ -1,12 +1,26 @@
 package com.draekk.springboot.springbootusersapi.controllers;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.draekk.springboot.springbootusersapi.models.User;
+import com.draekk.springboot.springbootusersapi.services.IUserService;
 
 @RestController
 @RequestMapping("/api")
 public class AppController {
 
-    
+    @Autowired
+    @Qualifier("userService")
+    private IUserService service;
+
+    @GetMapping({"/userlist", "/userlist/"})
+    public List<User> userlist() {
+        return service.findAll();
+    }
 }
