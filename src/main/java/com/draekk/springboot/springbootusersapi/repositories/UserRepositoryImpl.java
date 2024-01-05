7449e704e5785cd44dcc437e66ceafef84014c0e
Draekk <rodriver1992@gmail.com>
2024-01-05 10:45:45 -0300

    Commented out unused code in UserRepositoryImpl.java

--- src/main/java/com/draekk/springboot/springbootusersapi/repositories/UserRepositoryImpl.java
+++ src/main/java/com/draekk/springboot/springbootusersapi/repositories/UserRepositoryImpl.java
@@ -1,25 +1,27 @@
 package com.draekk.springboot.springbootusersapi.repositories;
 
 import java.util.ArrayList;
-import java.util.Arrays;
+// import java.util.Arrays;
 import java.util.List;
 
 import org.springframework.stereotype.Repository;
 
-import com.draekk.springboot.springbootusersapi.models.Account;
-import com.draekk.springboot.springbootusersapi.models.Address;
+// import com.draekk.springboot.springbootusersapi.models.Account;
+// import com.draekk.springboot.springbootusersapi.models.Address;
 import com.draekk.springboot.springbootusersapi.models.User;
 
 @Repository("userRepository")
 public class UserRepositoryImpl implements IUserRepository {
 
-    private List<User> users = new ArrayList<>(Arrays.asList(
-        new User(1L, "311513-2", "Gever", "Rodríguez", new Account(1L, "drakkseid", "THEHELL9000."), "rodriver1992@gmail.com", new Address(1L, "Jehova Yireh 1161", "Curacaví", "Chile")),
-        new User(2L, "235624-1", "Laura", "Peralta", new Account(2L, "aluranyx", "qrgqrgjri3."), "alura.peralta.02@gmail.com", new Address(2L, "Jehova Yireh 1161", "Curacaví", "Chile")),
-        new User(3L, "237624-1", "Andres", "Peralta", new Account(3L, "aluranyx", "qrgqrgjri3."), "andres@correo.cl", new Address(3L, "Jehova Yireh 1161", "Curacaví", "Chile")),
-        new User(4L, "314513-2", "Andrea", "Rodríguez", new Account(1L, "drakkseid", "THEHELL9000."), "rodriver1992@gmail.com", new Address(1L, "Jehova Yireh 1161", "Curacaví", "Chile")),
-        new User(5L, "144513-2", "Pablo", "Bustamante", new Account(1L, "pblb", "THEHELL9000."), "rodriver1992@gmail.com", new Address(1L, "Jehova Yireh 1161", "Curacaví", "Chile"))
-    ));
+    // private List<User> users = new ArrayList<>(Arrays.asList(
+    //     new User(1L, "311513-2", "Gever", "Rodríguez", new Account(1L, "drakkseid", "THEHELL9000."), "rodriver1992@gmail.com", new Address(1L, "Jehova Yireh 1161", "Curacaví", "Chile")),
+    //     new User(2L, "235624-1", "Laura", "Peralta", new Account(2L, "aluranyx", "qrgqrgjri3."), "alura.peralta.02@gmail.com", new Address(2L, "Jehova Yireh 1161", "Curacaví", "Chile")),
+    //     new User(3L, "237624-1", "Andres", "Peralta", new Account(3L, "aluranyx", "qrgqrgjri3."), "andres@correo.cl", new Address(3L, "Jehova Yireh 1161", "Curacaví", "Chile")),
+    //     new User(4L, "314513-2", "Andrea", "Rodríguez", new Account(1L, "drakkseid", "THEHELL9000."), "rodriver1992@gmail.com", new Address(1L, "Jehova Yireh 1161", "Curacaví", "Chile")),
+    //     new User(5L, "144513-2", "Pablo", "Bustamante", new Account(1L, "pblb", "THEHELL9000."), "rodriver1992@gmail.com", new Address(1L, "Jehova Yireh 1161", "Curacaví", "Chile"))
+    // ));
+
+    private List<User> users = new ArrayList<>();
 
     @Override
     public void save(User user) {
